35e033cab20e83dc80d5b00ece3c5e146117bce3
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-01-15 12:46:47 -0300

    adjusts in logic of main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
-function calculoIMC() {
+const form = document.getElementById('formMain');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     const valuePeso = document.getElementById('inputPeso').value
     const valueAltura = document.getElementById('inputAltura').value
     const erroAlerta = document.getElementById('alert-err');
 
+
     const valConverPeso = parseInt(valuePeso);
     const valConverAltura = parseFloat(valueAltura);
 
+    console.log(valuePeso);
+    console.log(valueAltura);
+
     if (!valuePeso || !valueAltura) {
         erroAlerta.classList.remove('visually-hidden');
+
         setTimeout(() => {
             erroAlerta.classList.add('visually-hidden')
         }, 3000)
@@ -21,41 +29,42 @@ function calculoIMC() {
             erroAlerta.classList.add('visually-hidden')
         }, 3000)
 
-    }
-
-    const resultIMC = valConverPeso / (valConverAltura * valConverAltura);
-
-    if (resultIMC <= 18.5) {
-        formatMessage('Você está abaixo do peso', 'alert-danger')
-
-    } else if (resultIMC > 18.5 && resultIMC <= 24.9) {
-        formatMessage('Você está no peso normal', 'alert-success')
-
-    } else if ( resultIMC > 24.9 && resultIMC <= 29.9) {
-        formatMessage('Você está no sobrepeso, cuidado!', 'alert-warning')
-
-    } else if ( resultIMC > 29.9 && resultIMC <= 34.9) {
-        formatMessage('Você está com obesidade grau 1, está na hora de se cuidar!', 'alert-danger')
-
-    } else if ( resultIMC > 34.9 && resultIMC <= 39.9) {
-        formatMessage('Você está com obesidade grau 2, está na hora de se cuidar!', 'alert-danger')
-
     } else {
-        formatMessage('Você está com obesidade grau 3, está na hora de se cuidar!', 'alert-danger')
 
+        const resultIMC = valConverPeso / (valConverAltura * valConverAltura);
+
+        if (resultIMC <= 18.5) {
+            formatMessage('Você está abaixo do peso', 'alert-danger')
+
+        } else if (resultIMC > 18.5 && resultIMC <= 24.9) {
+            formatMessage('Você está no peso normal', 'alert-success')
+
+        } else if (resultIMC > 24.9 && resultIMC <= 29.9) {
+            formatMessage('Você está no sobrepeso, cuidado!', 'alert-warning')
+
+        } else if (resultIMC > 29.9 && resultIMC <= 34.9) {
+            formatMessage('Você está com obesidade grau 1, está na hora de se cuidar!', 'alert-danger')
+
+        } else if (resultIMC > 34.9 && resultIMC <= 39.9) {
+            formatMessage('Você está com obesidade grau 2, está na hora de se cuidar!', 'alert-danger')
+
+        } else {
+            formatMessage('Você está com obesidade grau 3, está na hora de se cuidar!', 'alert-danger')
+
+        }
     }
-}
 
-function formatMessage(message, classe){
-    const erroAlerta = document.getElementById('alert-err');
+    function formatMessage(message, classe) {
+        const erroAlerta = document.getElementById('alert-err');
 
-    erroAlerta.innerText = ' ';
+        erroAlerta.innerText = ' ';
         erroAlerta.innerText = message
         erroAlerta.classList.remove('visually-hidden')
-        erroAlerta.classList.replace('alert-success' , classe)
+        erroAlerta.classList.replace('alert-success', classe)
 
         setTimeout(() => {
             erroAlerta.classList.add('visually-hidden')
-            erroAlerta.classList.replace(classe , 'alert-success')
+            erroAlerta.classList.replace(classe, 'alert-success')
         }, 3000)
-}
+    }
+})
